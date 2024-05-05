3aac6d450968495337ee9b5d0bc0f8e10f30c4a0
Aira <a.solanop@uniandes.edu.co>
2024-05-05 17:19:30 -0500

    Merge con main

--- KRAKEN/features/web/step_definitions/step.js
+++ KRAKEN/features/web/step_definitions/step.js
@@ -132,6 +132,21 @@ When('I click on save member', async function () {
     return await element.click();
 });
 
+When('I click on search', async function(){
+    let element = await this.driver.$('body.ember-application > div.gh-app > div.gh-viewport > nav.gh-nav > div.flex.flex-column.h-100 > header.gh-nav-menu > div.gh-nav-menu-search > button.gh-nav-btn-search')
+    return await element.click();
+});
+
+When('I enter post name {kraken-string}', async function (value) {
+    let element = await this.driver.$('body.ember-application.epm-scrolling-disabled > div.epm-modal-container > div.epm-modal.fullscreen-modal-action.fullscreen-modal-wide > div.modal-content > div.gh-nav-search-modal > div.gh-nav-search-input > div.ember-basic-dropdown.ember-power-select-search > div.ember-view.ember-basic-dropdown-trigger.ember-basic-dropdown-trigger--in-place.ember-power-select-trigger > input');
+    //return await element.click();
+    return await element.setValue(value);
+});
+
+Then('I should see the post coming soon', async function () {
+    let element = await this.driver.$('ul.ember-power-select-options > li.ember-power-select-group > ul.ember-power-select-options > li.ember-power-select-option');
+    return await element.click();
+});
 
 Then('I should see the all authors option', async function () {
     let element = await this.driver.$('#ember-basic-dropdown-wormhole > div > ul > li:nth-child(1)');
