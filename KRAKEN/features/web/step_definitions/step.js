70402e9a00a0da664e2aeeb6e9c038e5097f100d
Aira <a.solanop@uniandes.edu.co>
2024-05-05 20:26:15 -0500

    Create new post

--- KRAKEN/features/web/step_definitions/step.js
+++ KRAKEN/features/web/step_definitions/step.js
@@ -142,20 +142,47 @@ When('I click on Drafts', async function(){
     return await element.click();
 });
 
-When('I click on Schedule', async function(){
-    let element = await this.driver.$('body.ember-application > div.gh-app > div.gh-viewport > nav.gh-nav > div.flex.flex-column.h-100 > section.gh-nav-body > div.gh-nav-top > ul:nth-child(2) > li:nth-child(1) > div.liquid-container.ember-view > div.liquid-child.ember-view > ul.gh-nav-view-list > li:nth-child(2)');
-    return await element.click();
-});
-
-When('I click on Published', async function(){
-    let element = await this.driver.$('body.ember-application > div.gh-app > div.gh-viewport > nav.gh-nav > div.flex.flex-column.h-100 > section.gh-nav-body > div.gh-nav-top > ul:nth-child(2) > li:nth-child(1) > div.liquid-container.ember-view > div.liquid-child.ember-view > ul.gh-nav-view-list > li:nth-child(3)');
-    return await element.click();
-});
-
 When('I enter post name {kraken-string}', async function (value) {
     let element = await this.driver.$('body.ember-application.epm-scrolling-disabled > div.epm-modal-container > div.epm-modal.fullscreen-modal-action.fullscreen-modal-wide > div.modal-content > div.gh-nav-search-modal > div.gh-nav-search-input > div.ember-basic-dropdown.ember-power-select-search > div.ember-view.ember-basic-dropdown-trigger.ember-basic-dropdown-trigger--in-place.ember-power-select-trigger > input');
     //return await element.click();
     return await element.setValue(value);
+});
+
+When('I write post title {kraken-string}', async function (value) {
+    let element = await this.driver.$('textarea.gh-editor-title.ember-text-area.gh-input.ember-view');
+    //return await element.click();
+    await element.setValue(value);
+    element = await this.driver.$('div.koenig-editor__editor-wrapper');
+    return await element.click();
+});
+When('I write post body {kraken-string}', async function (value) {
+    let element = await this.driver.$('div.koenig-editor__editor-wrapper');
+    await element.click();
+    return await element.setValue(value);
+});
+
+When('I click on SortBy', async function(){
+    let element = await this.driver.$('div.gh-contentfilter-menu.gh-contentfilter-sort');
+    return await element.click();
+});
+
+When('I click on NewPost', async function(){
+    let element = await this.driver.$('a.ember-view.gh-secondary-action.gh-nav-new-post');
+    return await element.click();
+});
+
+Then('I publish post', async function () {
+    let element = await this.driver.$('button.gh-btn.gh-btn-editor.darkgrey.gh-publish-trigger');
+    return await element.click();
+});
+
+Then('I click PublishPost', async function () {
+    let element = await this.driver.$('button.gh-btn.gh-btn-large.gh-btn-pulse.ember-view');
+    return await element.click();
+});
+Then('I click continue', async function () {
+    let element = await this.driver.$('button.gh-btn.gh-btn-black.gh-btn-large');
+    return await element.click();
 });
 
 Then('I should see the post coming soon', async function () {
@@ -248,6 +275,3 @@ Then('I should get an error message that specifies that the member already exist
     let element = await this.driver.$('body.ember-application > div.gh-app > div.gh-viewport > main.gh-main > section.gh-canvas > div > form.member-basic-info-form > div.gh-member-settings > section.gh-main-section.columns-3 > div.gh-main-section-block.span-2 > div.gh-main-section-content.grey > div > div.gh-cp-member-email-name > div.form-group.max-width.error > p.response');
     return await element.click();
 });
-
-
-
