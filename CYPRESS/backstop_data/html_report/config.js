937704dc207c873c7b06f507339d911c4f3d1c38
juanR <juan.romero@masiv.com>
2024-05-12 00:55:18 -0500

    add backstop

---
+++ CYPRESS/backstop_data/html_report/config.js
@@ -0,0 +1,131 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "..\\..\\cypress\\screenshots\\data\\version3_42_0_blackstop\\ghost_vrt_tests_Crear_nueva_pagina_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20240512-005057\\ghost_vrt_tests_Crear_nueva_pagina_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "ghost_vrt_tests_Crear_nueva_pagina_0_document_0_desktop.png",
+        "label": "Crear nueva pagina",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 4,
+        "url": "https://ghost-a76u.onrender.com/ghost/#/editor/page",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.022916666666666665,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 20
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\..\\cypress\\screenshots\\data\\version3_42_0_blackstop\\ghost_vrt_tests_Filtros_autores_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20240512-005057\\ghost_vrt_tests_Filtros_autores_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "ghost_vrt_tests_Filtros_autores_0_document_0_desktop.png",
+        "label": "Filtros autores",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 4,
+        "url": "https://ghost-a76u.onrender.com/ghost/#/posts?type=published",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.02354166666666667,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 20
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\..\\cypress\\screenshots\\data\\version3_42_0_blackstop\\ghost_vrt_tests_Crear_nueva_pagina_y_mostrar_preview_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20240512-005057\\ghost_vrt_tests_Crear_nueva_pagina_y_mostrar_preview_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "ghost_vrt_tests_Crear_nueva_pagina_y_mostrar_preview_0_document_0_desktop.png",
+        "label": "Crear nueva pagina y mostrar preview",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 4,
+        "url": "https://ghost-a76u.onrender.com/ghost/#/editor/page",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.02,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 22
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\..\\cypress\\screenshots\\data\\version3_42_0_blackstop\\ghost_vrt_tests_Crear_nuevo_post_y_publicarlo_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20240512-005057\\ghost_vrt_tests_Crear_nuevo_post_y_publicarlo_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "ghost_vrt_tests_Crear_nuevo_post_y_publicarlo_0_document_0_desktop.png",
+        "label": "Crear nuevo post y publicarlo",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 4,
+        "url": "https://ghost-a76u.onrender.com/ghost/#/posts",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.02395833333333333,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 21
+        }
+      },
+      "status": "pass"
+    },
+    {
+      "pair": {
+        "reference": "..\\..\\cypress\\screenshots\\data\\version3_42_0_blackstop\\ghost_vrt_tests_Validate_Recent_Filters_Pages_0_document_0_desktop.png",
+        "test": "..\\bitmaps_test\\20240512-005057\\ghost_vrt_tests_Validate_Recent_Filters_Pages_0_document_0_desktop.png",
+        "selector": "document",
+        "fileName": "ghost_vrt_tests_Validate_Recent_Filters_Pages_0_document_0_desktop.png",
+        "label": "Validate Recent Filters Pages",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 4,
+        "url": "https://ghost-a76u.onrender.com/ghost/#/pages",
+        "expect": 0,
+        "viewportLabel": "desktop",
+        "diff": {
+          "isSameDimensions": true,
+          "dimensionDifference": {
+            "width": 0,
+            "height": 0
+          },
+          "rawMisMatchPercentage": 0.02375,
+          "misMatchPercentage": "0.02",
+          "analysisTime": 20
+        }
+      },
+      "status": "pass"
+    }
+  ],
+  "id": "ghost_vrt_tests"
+});
